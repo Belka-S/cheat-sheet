1932508c89068d53f4ccaa2fedd885d1ad28ea96
Belka-S <belka.srg@gmail.com>
2023-02-01 23:52:08 +0100

    add js_5-js_7

--- js/task-class/js_05.js
+++ js/task-class/js_05.js
@@ -1,7 +1,5 @@
 // # Module 3. Lesson 1. Objects
 
-// <!-- https://github.com/luxplanjay/js-33-qna/blob/03-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D1%8B/js/vehicles.js -->
-
 // ## ******************** Example 1 - Object Basics ********************
 
 // Write a script that, for the `user` object, successively:
@@ -11,14 +9,16 @@
 // - prints the contents of the `user` object in `key:value` format using
 //   `Object.keys()` and `for...of`
 
-// const user = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'html',
-//   premium: true,
-// };
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
 
-// -----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------- // user.mood = 'happy'; user.hobby = 'skydiving'; user.premium = false; for (const key of Object.keys(user)) {console.log(`${key} : ${user[key]}`);}
+
+// console.table(user);
 
 // ## ******************** Example 2 -  Object.values() method ********************
 
@@ -26,222 +26,162 @@
 // summing all salaries and store the result in the sum variable. Should
 // get 390. If the `salaries` object is empty, then the result should be 0.
 
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
+const salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
 
-// -----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------- // let sum = 0; for (const name of Object.keys(salaries)) {sum += salaries[name];}
+
+// console.log('sum: ', sum);
 
 // ## ******************** Example 3 - Array of objects ********************
 
-// Write a function `calcTotalPrice(stones, stoneName)` that takes an array
-// of objects and a string with the name of the stone. The function calculates and returns the total cost
-// of stones with the same name, price and quantity from the object
+// Write a function `calcTotalPrice(stones, stoneName)` that takes an array of objects
+// and a string with the name of the stone.The function calculates and returns the
+// total cost of stones with the same name, price and quantity from the object.
 
-// const stones = [
-//   { name: 'Emerald', price: 1300, quantity: 4 },
-//   { name: 'Diamond', price: 2700, quantity: 3 },
-//   { name: 'Sapphire', price: 400, quantity: 7 },
-//   { name: 'Rubble', price: 200, quantity: 2 },
-// ];
+const array = [
+  { name: 'Emerald', price: 1300, quantity: 4 },
+  { name: 'Diamond', price: 2700, quantity: 3 },
+  { name: 'Sapphire', price: 400, quantity: 7 },
+  { name: 'Rubble', price: 200, quantity: 2 },
+];
 
-// -----------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------- // function calcTotalPrice(stones, stoneName) {for (const stone of stones) {if (stone.name === stoneName) {return console.log(stone.price * stone.quantity);}}}
+
+function calcTotalPrice(stones, stoneName) {}
+
+calcTotalPrice(array, 'Sapphire');
 
 // ## ******************** Example 4 - Complex tasks ********************
 
 // Write a script for managing a personal account of an Internet bank. There is an `account` object
 // in which it is necessary to implement methods for working with balance and transaction history.
-
-// /*
 //  * There are only two types of transaction.
 //  * You can deposit or withdraw money from your account.
-//  */
-// const Transaction = {
-//   DEPOSIT: 'deposit',
-//   WITHDRAW: 'withdraw',
-// };
-
-// /*
-//  * Each transaction is an object with properties: id, type and amount
-//  */
-
 // const account = {
-//   // Current account balance
 //   balance: 0,
-
-//   // Transaction History
 //   transactions: [],
-
-//   /*
-//    * Method creates and returns a transaction object.
-//    * Accepts amount and type of transaction.
-//    */
 //   createTransaction(amount, type) {},
-
-//   /*
-//    * The method responsible for adding the amount to the balance..
-//    * Accepts the amount of the transaction.
-//    * Calls createTransaction to create a transaction object
-//    * then adds it to the transaction history
-//    */
 //   deposit(amount) {},
-
-//   /*
-//    *The method responsible for withdrawing the amount from the balance.
-//    * Принимает сумму танзакции.
-//    * Calls createTransaction to create a transaction object
-//    * then adds it to the transaction history.
-//    *
-//    * If amount is greater than the current balance, display a message
-//    * about the fact that the withdrawal of such an amount is not possible, there are not enough funds.
-//    */
 //   withdraw(amount) {},
-
-//   /*
-//    * The method returns the current balance
-//    */
 //   getBalance() {},
-
-//   /*
-//    * The method searches and returns the transaction object by id
-//    */
-//   getTransactionDetails(id) {},
-
-//   /*
-//    * The method returns the amount of funds
-//    *a specific type of transaction from the entire history of transactions
-//    */
-//   getTransactionTotal(type) {},
+//   find(id) {},
+//   getTotal(type) {},
 // };
+const accountExample = {
+  // Current account balance
+  balance: 0,
+  // Transaction History
+  transactions: [],
 
-// -----------------------------------------------------------------------------
+  /*
+   * Method creates and returns a transaction object.
+   * Accepts amount and type of transaction.
+   */
+  createTransaction(amount, type) {},
 
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+  /*
+   * The method responsible for adding the amount to the balance..
+   * Accepts the amount of the transaction.
+   * Calls createTransaction to create a transaction object
+   * then adds it to the transaction history
+   */
+  deposit(amount) {},
 
-// console.log("Модуль 3. Заняття 1. Об'єкти");
+  /*
+   *The method responsible for withdrawing the amount from the balance.
+   * Принимает сумму танзакции.
+   * Calls createTransaction to create a transaction object
+   * then adds it to the transaction history.
+   *
+   * If amount is greater than the current balance, display a message
+   * about the fact that the withdrawal of such an amount is not possible, there are not enough funds.
+   */
+  withdraw(amount) {},
 
-// ## ******************** Example 1 - Основи об'єктів ********************
+  /*
+   * The method returns the current balance
+   */
+  getBalance() {},
 
-// Напиши скрипт, який для об'єкта `user`, послідовно:
-// - додає поле `mood` зі значенням `'happy'`
-// - замінює значення `hobby` на `'skydiving'`
-// - замінює значення `premium` на `false`
-// - виводить вміст об'єкта `user` у форматі `ключ:значення` використовуючи
-//   `Object.keys()` та `for...of`
-// const user = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'html',
-//   premium: true,
-// };
+  /*
+   * The method searches and returns the transaction object by id
+   */
+  find(number) {},
 
-// user.mood = 'happy';
-// user.hobby = 'skydiving';
-// user.premium = false;
-// console.table(user);
+  /*
+   * The method returns the amount of funds
+   *a specific type of transaction from the entire history of transactions
+   */
+  getTotal(type) {},
+};
+//  * Each transaction is an object with properties: id, type and amount
+const transaction = {
+  DEPOSIT: 'deposit',
+  WITHDRAW: 'withdraw',
+};
 
-// for (const key of Object.keys(user)) {
-//   console.log(`${key}: ${user[key]}`);
-// }
+// -------------------------------------------------------------------------------------------- //
 
-// ##******************** Example 2 - метод Object.values() ********************
+const account = {
+  balance: 0,
+  transactions: [],
+  createTransaction(amount, type) {},
+  deposit(amount) {},
+  withdraw(amount) {},
+  getBalance() {},
+  find(id) {},
+  getTotal(type) {},
+};
 
-// У нас є об'єкт, де зберігаються зарплати нашої команди. Напишіть код для
-// підсумовування всіх зарплат і збережіть результат у змінній sum. Повинно
-// вийти 390. Якщо об'єкт `salaries` порожній, то результат має бути 0.
-// const salaries = {
-//   John: 100,
-//   Ann: 160,
-//   Pete: 130,
-// };
+account.deposit(100);
+account.withdraw(60);
+account.deposit(200);
+account.withdraw(80);
+account.getBalance();
+account.find(3);
+account.find(9);
+account.getTotal('withdraw');
+// console.table(account.transactions);
 
-// let sum = 0;
-// for (const salary of Object.values(salaries)) {
-//   sum += salary;
-// }
-// console.log('sum: ', sum);
-
-// ##******************** Example 3 - Масив об'єктів ********************
-
-// Напишіть функцію `calcTotalPrice(stones, stoneName)`, яка приймає масив
-// об'єктів та рядок з назвою каменю. Функція рахує і повертає загальну вартість
-// каміння з таким ім'ям, ціною та кількістю з об'єкта
-// const stones = [
-//   { name: 'Смарагд', price: 1300, quantity: 4 },
-//   { name: 'Діамант', price: 2700, quantity: 3 },
-//   { name: 'Сапфір', price: 400, quantity: 7 },
-//   { name: 'Щебінь', price: 200, quantity: 2 },
-// ];
-
-// function calcTotalPrice(stones, stoneName) {
-//   for (const stone of stones) {
-//     if (stone.name === stoneName)
-//       return console.log(stone.price * stone.quantity);
-//   }
-//   console.log(`Камінь відсутній`);
-// }
-// calcTotalPrice(stones, 'Смарагд');
-
-// ##******************** Example 4 - Комплексне завдання ********************
-
-// Напиши скрипт управління особистим кабінетом інтернет банку. Є об'єкт `account`
-// в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій.
-//  * Типів транзакцій всього два. Можна покласти чи зняти гроші з рахунку.
-
-//  * Метод, що відповідає за додавання суми до балансу. Приймає суму транзакції.
-//  * Викликає createTransaction для створення об'єкта транзакції
-//  * після чого додає його до історії транзакцій
-
-//  * Метод, що відповідає за зняття суми з балансу. Приймає суму транзакції.
-//  * Викликає createTransaction для створення об'єкта транзакції після чого додає
-//  * його до історії транзакцій. Якщо amount більше ніж поточний баланс, виводь
-//  * повідомлення про те, що зняття такої суми не можливе, недостатньо коштів.
-
-//  * Метод шукає та повертає об'єкт транзакції по id
-//  * Метод повертає кількість коштів певного типу транзакції з усієї історії транзакцій
-
+// -------------------------------------------------------------------------------------------- //
 // const account = {
 //   balance: 0,
-//   transactionId: 0,
-//   transactionTypes: { deposit: 'deposit', withdraw: 'withdraw' },
+//   num: 1,
 //   transactions: [],
-
-//   createTransaction(amount, type) {},
-
-//   deposit(amount) {},
-
-//   withdraw(amount) {},
-
-//   get history() {},
-
-//   find(id) {},
-
-//   total(type) {},
+//   createTransaction(amount, type) {
+//     this.transactions.push({ number: this.num, [type]: amount });
+//   },
+//   deposit(amount) {
+//     this.createTransaction(amount, transaction.DEPOSIT);
+//     this.balance += amount;
+//     this.num += 1;
+//   },
+//   withdraw(amount) {
+//     if (this.balance > 0) {
+//       this.createTransaction(amount, transaction.WITHDRAW);
+//       this.balance -= amount;
+//       this.num += 1;
+//     }
+//   },
+//   getBalance() {
+//     console.log(this.balance);
+//   },
+//   find(number) {
+//     if (number <= this.num) {
+//       console.log(this.transactions.at(number - 1));
+//     }
+//   },
+//   getTotal(type) {
+//     let total = 0;
+//     for (const transaction of this.transactions) {
+//       if (transaction[type]) {
+//         total += transaction[type];
+//       }
+//     }
+//     console.log(`Total ${type} = ${total}`);
+//   },
 // };
-
-// console.table(account);
-// account.deposit(100);
-// account.withdraw(60);
-// account.deposit(200);
-// account.withdraw(80);
-// account.history;
-// account.find(3);
-// account.find(9);
-// account.total('deposit');
--- js/task-class/js_06.js
+++ js/task-class/js_06.js
@@ -5,123 +5,93 @@
 // Rewrite the function so that it takes one object of the parameter instead of a set
 // of independent arguments.
 
-// function calcBMI(weight, height) {
-//   const numericWeight = Number(weight.replace(',', '.'));
-//   const numericHeight = Number(height.replace(',', '.'));
-//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
-// }
+// -------------------------------------------------------------------------------------------- // function calcBMI({weight, height}) {}
 
-// // It was
-// // console.log(calcBMI('88,3', '1.75'));
-// // console.log(calcBMI('68,3', '1.65'));
-// // console.log(calcBMI('118,3', '1.95'));
+function calcBMI(weight, height) {
+  const numericWeight = Number(weight.replace(',', '.'));
+  const numericHeight = Number(height.replace(',', '.'));
+  // console.log(Number((numericWeight / numericHeight ** 2).toFixed(1)));
+}
 
-// // Is expected
-// console.log(
-//   calcBMI({
-//     weight: '88,3',
-//     height: '1.75',
-//   }),
-// );
-// console.log(
-//   calcBMI({
-//     weight: '68,3',
-//     height: '1.65',
-//   }),
-// );
-// console.log(
-//   calcBMI({
-//     weight: '118,3',
-//     height: '1.95',
-//   }),
-// );
+// calcBMI({
+//   weight: '88,3',
+//   height: '1.75',
+// });
 
-// -----------------------------------------------------------------------------
+// calcBMI({
+//   weight: '68,3',
+//   height: '1.65',
+// });
+
+// calcBMI({
+//   weight: '118,3',
+//   height: '1.95',
+// });
 
 // ## ******************** Example 2 - Destructuring ********************
 
 // Rewrite the function so that it takes one object of the parameter instead of a set
 // of independent arguments.
 
-// function printContactsInfo(names, phones) {
-//   const nameList = names.split(',');
-//   const phoneList = phones.split(',');
-//   for (let i = 0; i < nameList.length; i += 1) {
-//     console.log(`${nameList[i]}: ${phoneList[i]}`);
-//   }
-// }
+// -------------------------------------------------------------------------------------------- // function printContactsInfo({ names, phones }) {}
 
-// // It was
-// // printContactsInfo(
-// //   'Jacob,William,Solomon,Artemis',
-// //   '89001234567,89001112233,890055566377,890055566300',
-// // );
+function printContactsInfo() {
+  const nameList = names.split(',');
+  const phoneList = phones.split(',');
+  for (let i = 0; i < nameList.length; i += 1) {
+    console.log(`${nameList[i]}: ${phoneList[i]}`);
+  }
+}
 
-// // Is expected
 // printContactsInfo({
 //   names: 'Jacob,William,Solomon,Artemis',
 //   phones: '89001234567,89001112233,890055566377,890055566300',
 // });
-
-// -----------------------------------------------------------------------------
 
 // ## ******************** Example 3 - Deep destructuring ********************
 
 // Rewrite the property so that it uses one object's parameter instead of a set
 // of independent arguments.
 
-// function getBotReport(companyName, repairBots, defenceBots) {
-//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
-// }
+// -------------------------------------------------------------------------------------------- //function getBotReport({companyName, bots: { repair: repairBots, defence: defenceBots },}) {}
 
-// // It was
-// // console.log(getBotReport('Cyberdyne Systems', 150, 50));
+function getBotReport(companyName, repairBots, defenceBots) {
+  // console.log(`${companyName} has ${repairBots + defenceBots} bots in stock`);
+}
 
-// // Is expected
-// console.log(
-//   getBotReport({
-//     companyName: 'Cyberdyne Systems',
-//     bots: {
-//       repair: 150,
-//       defence: 50,
-//     },
-//   }),
-// ); // "Cyberdyne Systems has 200 bots in stock"
-
-// -----------------------------------------------------------------------------
+getBotReport({
+  companyName: 'Cyberdyne Systems',
+  bots: {
+    repair: 150,
+    defence: 50,
+  },
+}); // "Cyberdyne Systems has 200 bots in stock"
 
 // ## ******************** Example 4 - Destructuring ********************
 
-// Rewrite the function so that it accepts a parameters object with properties
-// `companyName` and `stock` and display a report abou the number of goods in the warehouse
-// of any companies.
+// Rewrite the function so that it accepts a parameters object with properties `companyName`
+// and `stock` and display a report about the number of goods in the warehouse of any companies.
 
-// function getStockReport() {
+// -------------------------------------------------------------------------------------------- // function getStockReport({ companyName, stock }) {let sum = 0; for (const key of Object.keys(stock)) {sum += stock[key];} console.log(`${companyName} has ${sum} item`);}
 
-// }
+function getStockReport() {}
 
-// console.log(
-//   getStockReport({
-//     companyName: 'Cyberdyne Systems',
-//     stock: {
-//       repairBots: 150,
-//       defenceBots: 50,
-//     },
-//   }),
-// ); // "Cyberdyne Systems has 200 items in stock"
+getStockReport({
+  companyName: 'Cyberdyne Systems',
+  stock: {
+    repairBots: 150,
+    defenceBots: 50,
+  },
+}); // "Cyberdyne Systems has 200 items in stock"
 
-// console.log(
-//   getStockReport({
-//     companyName: 'Belacci',
-//     stock: {
-//       shoes: 20,
-//       skirts: 10,
-//       hats: 5,
-//     },
-//   }),
-// ); // "Belacci has 35 item in stock"
-
-// -----------------------------------------------------------------------------
+getStockReport({
+  companyName: 'Belacci',
+  stock: {
+    shoes: 20,
+    skirts: 10,
+    hats: 5,
+  },
+}); // "Belacci has 35 item in stock"
 
 // ## ******************** Example 5 - operation Spread ********************
 
@@ -129,38 +99,37 @@
 // contact object with `id` and `createdAt` properties added, as well as `list` with
 // value "default" if there is no such property in `partialContact`.
 
-// function createContact() {
+function generateId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
 
-// }
+// -------------------------------------------------------------------------------------------- // function createContact({ list = 'default', ...rest }) {return { ...rest, list, id: generateId(), createdAt: Date.now() };}
 
-// console.log(
+function createContact() {}
+
+// console.table(
 //   createContact({
 //     name: 'Mango',
 //     email: 'mango@mail.com',
 //     list: 'friends',
-//   }),
+//   })
 // );
-// console.log(
+
+// console.table(
 //   createContact({
 //     name: 'Poly',
 //     email: 'poly@hotmail.com',
-//   }),
+//   })
 // );
-
-// function generateId() {
-//   return '_' + Math.random().toString(36).substr(2, 9);
-// }
-
-// -----------------------------------------------------------------------------
 
 // ## ******************** Example 6 - Operation rest ********************
 
 // Write a `transformUsername(user)` function to return a new object
 // with `fullName` property instead of `firstName` and `lastName`.
 
-// function transformUsername() {
+// -------------------------------------------------------------------------------------------- // function transformId({ firstName, lastName, ...rest }) {return { fullName: `${firstName} ${lastName}`, ...rest };}
 
-// }
+function transformId() {}
 
 // console.log(
 //   transformId({
@@ -169,7 +138,7 @@
 //     lastName: 'Mercer',
 //     email: 'j.mercer@mail.com',
 //     friendCount: 40,
-//   }),
+//   })
 // );
 
 // console.log(
@@ -179,192 +148,5 @@
 //     lastName: 'Cross',
 //     email: 'a.cross@hotmail.com',
 //     friendCount: 20,
-//   }),
-// );
-
-// -----------------------------------------------------------------------------
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-// console.log('Модуль 3 Заняття 6. Деструктуризація та rest/spread');
-
-// ##********************Example 1 - Деструктуризація ********************
-
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
-// function calcBMI(weight, height) {
-//   const numericWeight = Number(weight.replace(',', '.'));
-//   const numericHeight = Number(height.replace(',', '.'));
-//   return Number((numericWeight / numericHeight ** 2).toFixed(1));
-// }
-// console.log(
-//   calcBMI({
-//     weight: '88,3',
-//     height: '1.75',
 //   })
 // );
-// console.log(
-//   calcBMI({
-//     weight: '68,3',
-//     height: '1.65',
-//   })
-// );
-// ##******************** Example 2 - Деструктуризація ********************
-
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
-// function printUser(name, surname, age) {
-//   console.log(`User is ${name} ${surname}, his age is ${age}`);
-// }
-// printUser('John', 'John', 24);
-// const user = {
-//   name: 'John',
-//   surname: 'Smith',
-// };
-// printUser(user);
-
-// ##******************** Example 3 - Глибока деструктуризація ********************
-
-// Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
-// незалежних аргументів.
-// function getBotReport(company) {
-// const {}=company
-//   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
-// }
-// const boatCompany = {
-//   company: 'Cyberdyne Systems',
-//   bots: {
-//     repair: 150,
-//     defence: 50,
-//   },
-// };
-// console.log(getBotReport(boatCompany)); // "Cyberdyne Systems has 200 bots in stock"
-
-// -----------------------------------------------------------------------------
-// const {
-//   company: companyName,
-//   bots: { repair: repairBots, defence: defenceBots },
-// } = company;
-
-// ##******************** Example 4 - Деструктуризація ********************
-
-// Напиши функцію, що приймає об'єкт параметрів із властивостями
-// `companyName` та `stock` та виводить звіт про кількість товарів на складі будь-якої
-// компанії.
-// function getStockReport() {}
-
-// const company = {
-//   companyName: 'Cyberdyne Systems',
-//   stock: {
-//     repairBots: 150,
-//     defenceBots: 50,
-//     defenceBots2: 50,
-//     defenceBots1: 50,
-//   },
-// };
-// getStockReport(company);
-// getStockReport({
-//   companyName: 'Cyberdyne Systems',
-//   stock: {
-//     repairBots: 150,
-//     defenceBots: 50,
-//   },
-// }); // "Cyberdyne Systems has 200 items in stock"
-
-// getStockReport({
-//   companyName: 'Belacci',
-//   stock: {
-//     shoes: 20,
-//     skirts: 10,
-//     hats: 5,
-//   },
-// }); // "Belacci has 35 item in stock"
-
-// -----------------------------------------------------------------------------
-// let sum = 0;
-// for (const item of Object.values(stock)) {
-//   sum += item;
-// }
-// console.log(`${companyName} has ${sum} items in stock`);
-// -----------------------------------------------------------------------------
-// const sum = Object.values(stock).reduce((acc, item) => {
-//   acc += item;
-//   return acc;
-// }, 0);
-// console.log(`${companyName} has ${sum} items in stock`);
-
-// ##******************** Example 5 - Операція spread ********************
-
-// Доповни функцію `createContact(partialContact)` так, щоб вона повертала новий
-// об'єкт контакту з доданими властивостями `id` та `createdAt`, а також `list` зі
-// значенням "default" якщо в `partialContact` немає такої властивості.
-// function createContact(partContact) {
-//   return {
-//     id: generateId(),
-//     createdAt: Date.now(),
-//     list,
-//   };
-// // }
-// const contact = {
-//   name: 'Mango',
-//   email: 'mango@mail.com',
-//   list: 'friends',
-// };
-// function generateId() {
-//   return '_' + Math.random().toString(36).substr(2, 9);
-// }
-// console.table(createContact(contact));
-
-// -----------------------------------------------------------------------------
-// function createContact(partContact) {
-//   return {
-//     list: 'default',
-//     ...partContact,
-//     id: generateId(),
-//     createdAt: Date.now(),
-//   };
-// }
-// -----------------------------------------------------------------------------
-// function createContact({ name, email, list = 'default' }) {
-//   return {
-//     name,
-//     email,
-//     list,
-//     id: generateId(),
-//     createdAt: Date.now(),
-//   };
-// }
-
-// ##******************** Example 6 - Операція rest ********************
-
-// Напиши функцію `transformUsername(user)` так, щоб вона повертала новий об'єкт із властивістю
-// `fullName`, замість `firstName` та `lastName`.
-// function transformUsername() {}
-// const user = {
-//   id: 1,
-//   firstName: 'Jacob',
-//   lastName: 'Mercer',
-//   email: 'j.mercer@mail.com',
-//   friendCount: 40,
-// };
-// console.table(transformUsername(user));
-
-// -----------------------------------------------------------------------------
-// function transformUsername({ firstName, lastName, ...rest }) {
-//   return { fullName: `${firstName} ${lastName}`, ...rest };
-// }
